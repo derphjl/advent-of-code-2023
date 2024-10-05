f749257768b193992c499a404777a203f61c01e3
flip <github@phjl.de>
2024-10-05 18:04:17 +0200

    need to reimplement (multiple simultaneous paths)

--- day8/main-part2.js
+++ day8/main-part2.js
@@ -2,7 +2,7 @@ import { readFile } from 'node:fs/promises';
 console.log('🎄 Day 8 Part 2 - 👻 Ghosts navigating the desert... 🎄\n');
 
 try {
-  const lines = (await readFile('./input.txt', { encoding: 'utf8' })).trim().split('\n'); //import file
+  const lines = (await readFile('./input-small-part2.txt', { encoding: 'utf8' })).trim().split('\n'); //import file
   let moves = lines.shift();   //first line gives the movement string
   moves = moves.split('');
   lines.shift();                //shift out emptyline
@@ -25,28 +25,7 @@ try {
   let currentIndex = nodes.findIndex((a) => a.start == currentStart);   //find the index position of the currentpos
   let stepCount = 0;
   
-  do{
-    for (let move of moves) {
-      let nextIndex;
-      let nextStart;
-      switch (move) {
-        case 'L':
-        nextStart = nodes[currentIndex].left;
-        nextIndex = nodes.findIndex((a) => a.start == nextStart);
-        break;
-        case 'R':
-        nextStart = nodes[currentIndex].right;
-        nextIndex = nodes.findIndex((a) => a.start == nextStart);
-        break;
-        default:
-        console.log(`Sorry, we are out.`);
-      }
-      currentIndex = nextIndex;
-      stepCount++;
-    }
-  } while (nodes[currentIndex].start != 'ZZZ');
-
-  console.log(stepCount);
+  //console.log(stepCount);
   console.log();
   
 } catch (error) {
