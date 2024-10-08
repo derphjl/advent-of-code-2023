595b2ad0a9e0b7a9797713b700d07824acebcbf5
flip <github@phjl.de>
2024-10-08 18:07:00 +0200

    minimal cosmetic changes

--- day8/main-part2.js
+++ day8/main-part2.js
@@ -3,7 +3,7 @@ console.log('🎄 Day 8 Part 2 - 👻 Ghosts navigating the desert... 🎄\n');
 let start = new Date();
 
 try {
-  const lines = (await readFile('./input-small-part2.txt', { encoding: 'utf8' })).trim().split('\n'); //import file
+  const lines = (await readFile('./input.txt', { encoding: 'utf8' })).trim().split('\n'); //import file
   let moves = lines.shift();   //first line gives the movement string
   moves = moves.split('');
   lines.shift();               //shift out emptyline
@@ -23,8 +23,7 @@ try {
     nodes.push(node);
   }
   
-  console.log('nodes');
-  console.log(nodes);
+  console.log(nodes.length + ' nodes in Input File');
   
   //### Select the "Start Nodes" -> All nodes that End in "A". Commit these to workingNodes[]
   let workingNodes = nodes.filter((node) => node.start.match(/^.*A$/g));
@@ -32,8 +31,11 @@ try {
   console.log(workingNodes);
   
   let stepCount = 0;
+  let maximumCount = 0;
+  let currentCount = 0;
   
   //### For all of the Start Nodes, step through all the movement instructions, one instruction at a time ###
+  console.log('Now moving and testing...');
   do {
     for (let move of moves) {
       stepCount++;
@@ -47,6 +49,11 @@ try {
         };
         workingNodes[i] = nodes.find((a) => a.start === nextStart);      
       }
+    }
+    currentCount = workingNodes.filter((a) => a.start.match(/^.*Z$/g)).length;
+    if (currentCount > maximumCount){
+      maximumCount = currentCount;
+      console.log(maximumCount + "/" + workingNodes.length + " done after step " + stepCount);
     }
   } while (workingNodes.filter((a) => a.start.match(/^.*Z$/g)).length < workingNodes.length);
   console.log(stepCount + ' Steps Moved, WorkingNodes now:');
